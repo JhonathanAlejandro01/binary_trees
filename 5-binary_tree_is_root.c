4cdf8c77e406dd3a756b511dc895bd1138e615c9
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-21 18:52:42 -0500

    add 5 Task check if a Root correcting validations try3

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -11,9 +11,6 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (!node)
 		return (0);
 
-	if (node->left && node->right)
-		return (1);
-	else
-		return (0);
+
 	return (!node->parent);
 }
