fed583e61b3a4bdd5bfac4adcdf7081d18784a5d
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-21 17:41:03 -0500

    add 2 Task Insert Node Right correcting Betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
- *
+ * binary_tree_insert_right - function that inserts a node as the \
+right-child of another node
+ * @parent: is a pointer to the node to insert the right-child in.
+ * @value: is the value to store in the new node.
+ * Return: A pointer to the created node, or NULL on failure.
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
@@ -20,7 +21,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	bt->n = value;
 	bt->parent = parent;
 
-	if(parent->right)
+	if (parent->right)
 	{
 		bt->right = parent->right;
 		bt->right->parent = bt;
