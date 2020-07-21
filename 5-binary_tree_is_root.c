6bf18a1ca4b17e2ca4c913b027d6ca82f6350ca5
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-21 18:35:07 -0500

    add 5 Task check if a Root try 4

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,17 +6,24 @@
  * Return: 1 if node is a root, otherwise 0.
  */
 
-int binary_tree_is_root(const binary_tree_t *node)
+_Bool check_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-
+	if (node->left)
+		check_root(node->left);
+	if (node->right)
+		check_root(node->right);
 	if (node->left && node->right)
 		return (1);
 	else if (node != node->parent)
 		return (0);
 	else
 		return (0);
+}
 
-	return (0);
+int binary_tree_is_root(const binary_tree_t *node)
+{
+
+	return (check_root(node));
 }
