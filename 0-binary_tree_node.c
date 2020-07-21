f7eb2eb40db5fb0eacd766c1d326c11373df043d
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-21 16:05:29 -0500

    add 1 Task New Node and adding print binary trees try with calloc

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *bt = NULL;
 
-	bt = realloc(bt, sizeof(binary_tree_t));
+	bt = calloc(sizeof(binary_tree_t));
 	if (!bt)
 		return (NULL);
 	bt->parent = parent;
