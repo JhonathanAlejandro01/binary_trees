5fa2fc20e79002f46d77f60041e1e709d5f918e8
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-22 09:03:22 -0500

    adding some prototypes in the Header

--- binary_trees.h
+++ binary_trees.h
@@ -26,10 +26,6 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e8505f89642b2a27f0927c35de36f295e1e846ba
 /* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
 
@@ -41,10 +37,6 @@ typedef struct binary_tree_s heap_t;
 
 
 /* The Prototypes */
-<<<<<<< HEAD
-=======
-
->>>>>>> e8505f89642b2a27f0927c35de36f295e1e846ba
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -53,8 +45,6 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
